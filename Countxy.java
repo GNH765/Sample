28de7c4c9971df5002af514510b09ff373e5c1db
GNH765 <80455742+GNH765@users.noreply.github.com>
2021-03-12 05:46:37 +0900

    sample

---
+++ Countxy.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class Countxy{
+  public static void main(String[] args){
+    int Inputxy[];
+
+
+    Inputxy j1=new Inputxy();
+    System.out.println("1");
+    j1.Input();
+}
+}
