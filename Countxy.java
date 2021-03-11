e5de3989d5f1f9e3ff6b8586a5780b8eb61905d7
GNH765 <80455742+GNH765@users.noreply.github.com>
2021-03-12 06:11:55 +0900

    説明＋

--- Countxy.java
+++ Countxy.java
@@ -2,11 +2,10 @@ import java.util.Arrays;
 import java.util.Scanner;
 class Countxy{
   public static void main(String[] args){
-    int Inputxy[];
 
 
     Inputxy j1=new Inputxy();
-    System.out.println("1");
+    System.out.println("要素の数を入力してください");
     j1.Input();
 }
 }
--- Inputxy.java
+++ Inputxy.java
@@ -7,17 +7,16 @@ public int[] Input(){
 
   Scanner scan=new Scanner(System.in);
   int count=scan.nextInt();
-
+System.out.println(count+"の要素の配列を作成します");
   var oo=new int[count];
 
       for(int i=0 ; i<count; i++){
-
-
       int kazu=scan.nextInt();
       oo[i]=kazu;
-System.out.println(oo[i]);
-}System.out.println(Arrays.toString(oo));
+}
+System.out.println(Arrays.toString(oo));
 Arrays.sort(oo);
+System.out.println("並べ替えます");
 System.out.println(Arrays.toString(oo));
 return this.oo;
     }}
